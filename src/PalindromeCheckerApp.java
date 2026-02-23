2b1c224dbb7c4ee36672352fe5f556a7413d710e
Anantgupta23 <unlostthunder@gmail.com>
2026-02-23 16:39:33 +0530

    UC3:palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,18 +2,18 @@ public class PalindromeCheckerApp {
     public static void main(String[] args) {
         String originalString = "madam";
 
-        // Variable to store the reversed string
         String reversedString = "";
 
-        // Reverse the string
         for (int i = originalString.length() - 1; i >= 0; i--) {
             reversedString = reversedString + originalString.charAt(i);
         }
+        System.out.println("Original string: " + originalString);
+        System.out.println("Reversed string: " + reversedString);
 
-        // Check if the original string equals the reversed string
         if (originalString.equals(reversedString)) {
             System.out.println(originalString + " is a palindrome.");
         } else {
             System.out.println(originalString + " is not a palindrome.");
+        }
     }
-
+}
