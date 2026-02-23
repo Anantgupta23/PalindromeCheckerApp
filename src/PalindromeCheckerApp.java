c9e6a47b641a66d424eb602834f6c6faa8afd6e2
Anantgupta23 <unlostthunder@gmail.com>
2026-02-23 16:46:17 +0530

    UC6:palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,24 +1,35 @@
+import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-            String originalString = "madam";
-            Stack<Character> stack = new Stack<>();
+        String originalString = "madam";
 
-            for (int i = 0; i < originalString.length(); i++) {
-                stack.push(originalString.charAt(i));
+        Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
+
+        for (int i = 0; i < originalString.length(); i++) {
+            char c = originalString.charAt(i);
+            stack.push(c);
+            queue.add(c);
+        }
+
+        boolean isPalindrome = true;
+
+        while (!stack.isEmpty() && !queue.isEmpty()) {
+            if (!stack.pop().equals(queue.remove())) {
+                isPalindrome = false;
+                break;
             }
+        }
 
-            String reversedString = "";
-            while (!stack.isEmpty()) {
-                reversedString = reversedString + stack.pop();
-            }
+        System.out.println("Original string: " + originalString);
 
-            System.out.println("Original string: " + originalString);
-            System.out.println("Reversed string: " + reversedString);
-
-            if (originalString.equals(reversedString)) {
-                System.out.println(originalString + " is a palindrome.");
-            } else {
-                System.out.println(originalString + " is not a palindrome.");
+        if (isPalindrome) {
+            System.out.println(originalString + " is a palindrome.");
+        } else {
+            System.out.println(originalString + " is not a palindrome.");
         }
     }
 }
