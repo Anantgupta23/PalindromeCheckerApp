e2b9d44a251dff93ce61f62ddfd09ff294720208
Anantgupta23 <unlostthunder@gmail.com>
2026-02-23 16:26:25 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,14 +1,12 @@
-public class UseCase3PalindromeCheckerApp {
+public class PalindromeChecker {
     public static void main(String[] args) {
         String originalString = "madam";
+
         String reversedString = "";
 
         for (int i = originalString.length() - 1; i >= 0; i--) {
             reversedString = reversedString + originalString.charAt(i);
         }
-
-        System.out.println("Original string: " + originalString);
-        System.out.println("Reversed string: " + reversedString);
 
         if (originalString.equals(reversedString)) {
             System.out.println(originalString + " is a palindrome.");
